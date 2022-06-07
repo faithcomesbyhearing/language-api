7240aec6e9734c60d1d97d83dc18477f2dbe5570
Brad Flood <bflood@keyholesoftware.com>
2022-06-07 16:32:48 -0700

    wire in GH token;fix issues with datamodel make

--- .devcontainer/initializeData.sh
+++ .devcontainer/initializeData.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "initialize.sh - 1607"
+
 cd datamodel
-export MYSQL="mysql -h localhost -P 3306 -u root -ppassword --local_infile=1"
-make external-mysql language-mysql
+git status
+# due to docker_compose configuration, the mysql container can be accessed by the service name, which is "db"
+export MYSQL="mysql -h db -P 3306 -u root -ppassword "
+export ROOT=/tmp
+make mysql-clean language-mysql
 
