0c6c45d8727ba754ebf0cab635c79dd80c39d0c7
Brad Flood <bflood@keyholesoftware.com>
2022-06-15 22:11:55 +0000

    add logger;implement filter on language get

--- language/controllers/language_get.go
+++ language/controllers/language_get.go
@@ -1,20 +1,33 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/faithcomesbyhearing/language-api/language/models"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/gorm"
 )
 
 // GET /language
 // Get all languages, filtered
 // FIXME: implement filter
 func FindLanguages(c *gin.Context) {
-	log.Print("FindLanguages")
+	log.Println("FindLanguages")
+	params, _ := url.ParseQuery(c.Request.URL.RawQuery)
+	fmt.Println(params)
+	fmt.Println(params["iso"][0])
 	var languages []models.Language
-	models.DB.Find(&languages)
+
+	var iso = params["iso"][0]
+	var db *gorm.DB = models.DB
+	if len(iso) > 0 {
+		db = db.Where("iso3 = ?", iso)
+	}
+	db.Find(&languages)
+
 	c.JSON(http.StatusOK, gin.H{"data": languages})
 }
--- language/models/setup.go
+++ language/models/setup.go
@@ -1,18 +1,35 @@
 package models
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"github.com/faithcomesbyhearing/language-api/language/util"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
 
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
 	var dsn = util.Getenv("MYSQL_CONNECT_STRING", "root:password@tcp(host.docker.internal:3306)/LANGUAGE?charset=utf8mb4&parseTime=True&loc=Local")
 	//log.Printf("MYSQL_CONNECT_STRING: %s", dsn)
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		panic("Failed to connect to database!")
